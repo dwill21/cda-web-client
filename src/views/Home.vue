94e52094eccea0cd115241f70b937cf409cdb4fb
ryandewillis <dewillisr@yahoo.com>
2020-04-01 15:56:09 -0500

    Add thumbnails to home page showcasing main services offered

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,27 @@
   <div>
     <Header></Header>
     <div id="bg"></div>
-    <div id="tagline">
-      <h2 style="font-size: 40px">Power your business development with data-driven decisions</h2>
-    </div>
+
+    <b-container id="main-content">
+      <b-row>
+        <b-col>
+          <h2 id="tagline">Power your business development with <span class="emphasis">data-driven decisions</span></h2>
+        </b-col>
+
+        <b-col cols="2"></b-col>
+
+        <b-col>
+          <b-card-group id="cards" columns>
+            <b-card title="Price to Win" :img-src="require('../assets/data.jpg')" img-alt="Data" overlay class="card"></b-card>
+
+            <b-card title="Competitive Intel" :img-src="require('../assets/charts.jpeg')" img-alt="Image" overlay class="card"></b-card>
+
+            <b-card title="Proposal Writing" :img-src="require('../assets/documents.jpeg')" img-alt="Image" overlay class="card"></b-card>
+          </b-card-group>
+        </b-col>
+      </b-row>
+    </b-container>
+
     <Footer></Footer>
   </div>
 </template>
@@ -12,12 +30,20 @@
 <script>
 import Header from '../components/Header.vue'
 import Footer from '../components/Footer.vue'
+import { BCard, BCardGroup, BCol, BContainer, BRow } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 export default {
   name: 'Home',
   components: {
     Header,
     Footer,
+    BCard,
+    BCardGroup,
+    BCol,
+    BContainer,
+    BRow,
   },
 }
 </script>
@@ -29,12 +55,34 @@ export default {
     background-color: #444e57;
   }
 
-  #tagline {
+  #main-content {
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    width: 60%;
+    width: 80%;
+  }
+
+  #tagline {
+    position: relative;
+    top: 50%;
+    transform: translateY(-50%);
+    text-align: left;
+    font-size:40px;
     color: white;
+  }
+
+  .emphasis {
+    color: mediumspringgreen;
+  }
+
+  #cards {
+    display: inline;
+  }
+
+  .card {
+    object-fit: cover;
+    color: white;
+    text-shadow: 1px 1px black;
   }
 </style>
