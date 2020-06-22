7fb3303f2711588c1ff595c2e38abb46decf8c87
ryandewillis <dewillisr@yahoo.com>
2020-06-22 00:09:28 -0500

    Name webpack chunks

--- src/router/index.js
+++ src/router/index.js
@@ -1,43 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter);
 
-const routes = [
+const routeOptions = [
   {
     path: '/',
     name: 'Home',
-    component: Home
   },
   {
     path: '/about',
     name: 'About',
-    component: () => import('../views/About.vue')
   },
   {
     path: '/services',
     name: 'Services',
-    component: () => import('../views/Services.vue')
   },
   {
     path: '/contact',
     name: 'Contact',
-    component: () => import('../views/Contact.vue')
-  },
-  /* No features ready yet that need user logged in
-  {
-    path: '/login',
-    name: 'Login',
-    component: () => import('../views/Login.vue')
-  },
-  {
-    path: '/sign-up',
-    name: 'Sign up',
-    component: () => import('../views/SignUp.vue')
-  },
-   */
-];
+  }
+]
+
+const routes = routeOptions.map(route => {
+  return {
+    ...route,
+    component: () => import(/* webpackChunkName: "[request]" */ `../views/${route.name}.vue`)
+  }
+});
 
 const router = new VueRouter({
   mode: 'history',
