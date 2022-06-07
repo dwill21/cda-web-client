1e61b64d485c510ddf35cb44abe54c9bb5628791
ryandewillis <dewillisr@yahoo.com>
2022-06-07 18:38:54 -0500

    WIP Recreate project

--- src/main.js
+++ src/main.js
@@ -1,20 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
 
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-
-import { VBHover } from 'bootstrap-vue';
-Vue.directive('b-hover', VBHover);
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  created() {
-    AOS.init()
-  },
-  el: '#app',
   router,
-  render: h => h(App)
-})
+  render: (h) => h(App),
+}).$mount('#app');
--- src/router/index.js
+++ src/router/index.js
@@ -1,38 +1,29 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import HomeView from '../views/HomeView.vue';
 
 Vue.use(VueRouter);
 
-const routeOptions = [
+const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
+    component: HomeView,
   },
   {
     path: '/about',
-    name: 'About',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
   },
-  {
-    path: '/services',
-    name: 'Services',
-  },
-  {
-    path: '/contact',
-    name: 'Contact',
-  }
-]
-
-const routes = routeOptions.map(route => {
-  return {
-    ...route,
-    component: () => import(/* webpackChunkName: "[request]" */ `../views/${route.name}.vue`)
-  }
-});
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
-export default router
+export default router;
